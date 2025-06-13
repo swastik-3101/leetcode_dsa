a5525938563359de29b30a6f850a56e552557818
swastik-3101 <swastikkr1908@gmail.com>
2025-06-14 00:03:18 +0530

    Time: 11 ms (49.10%) | Memory: 16.2 MB (39.87%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        int n = s.length();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true; // empty string is always valid
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dict.find(s.substr(j, i - j)) != dict.end()) {
+                    dp[i] = true;
+                    break;  // no need to check more j's
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
