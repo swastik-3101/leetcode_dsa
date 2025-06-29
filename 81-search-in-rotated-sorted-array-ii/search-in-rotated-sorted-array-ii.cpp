c85555150dae0a660bd371b9f7052f4a8320081f
swastik-3101 <swastikkr1908@gmail.com>
2025-06-29 19:32:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (55.13%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int low =0;
+        int high = nums.size()-1;
+        while(low<=high){
+            int mid = (low + high)/2;
+            if(nums[mid]==target){
+                return true;
+            }
+            //skip karo 
+            if (nums[low] == nums[mid] && nums[high] == nums[mid]) {
+                low++;
+                high--;
+            }
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>target){
+                    high = mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target &&nums[high]>=target){
+                    low=mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+};
