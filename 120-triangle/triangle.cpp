5662f2d99f818ec00af90b8ca6114bb8fb2d309a
swastik-3101 <swastikkr1908@gmail.com>
2025-07-26 06:57:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (7.11%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,12 +2,20 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<int> dp = triangle.back();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        
+        for (int j = 0; j < n; j++) {
+            dp[n - 1][j] = triangle[n - 1][j];
+        }
+
+        
         for (int i = n - 2; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
-                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+                dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
             }
         }
-        return dp[0];
+
+        return dp[0][0];
     }
 };
